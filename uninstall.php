f6be719cf11ef198418cef646abdf483cace624e
emicha <emichaaa@gmail.com>
2021-01-31 20:23:13 +0200

    Delete plugin page on uninstall

--- uninstall.php
+++ uninstall.php
@@ -14,7 +14,7 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 $news = get_posts( array( 'post_type' => 'news', 'numberposts' => -1 ) );
 
 $plugin_page = get_page_by_title( 'News plugin page' );
-$news = array_merge( $news, $plugin_page );
+wp_delete_post( $plugin_page->ID, true );
 
 foreach( $news as $singleNews ) {
     wp_delete_post( $singleNews->ID, true );
